369c897874bb25454a27e4e09a86dd1158e4e136
Minh Nguyen <nhtminh2999@gmail.com>
2022-05-12 07:50:11 +0700

    commit

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -24,6 +24,7 @@ class AuthController {
       // Config mail server
       const transporter = nodemailer.createTransport({
         service: 'gmail',
+
         auth: {
           user: process.env.EMAIL,
           pass: process.env.EMAIL_PASSWORD
