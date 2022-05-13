8bc28836575882ae9dfe5708f1e9ad1d18ea2753
Minh Nguyen <nhtminh2999@gmail.com>
2022-05-13 09:21:25 +0700

    update cors

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
